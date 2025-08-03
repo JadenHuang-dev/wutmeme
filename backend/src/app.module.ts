5c34922aa8439586f7853de5579e7bfe240fb838
JadenHuang-dev <tttacos2000@gmail.com>
2025-08-04 00:56:42 +0900

    update: denpendency and first module setting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Meme } from './entities/meme.entity'; 
+import { Submission } from './entities/submission.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Meme, Submission],
+      synchronize: true, // dev: true，production: false
+    }),
+
+    TypeOrmModule.forFeature([Meme, Submission]),
+  ],
 })
 export class AppModule {}
